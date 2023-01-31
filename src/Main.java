c9c2fe1aa7975a3b9d117f1c201289c36106f656
Abaevsk94 <abaevsk@gmail.com>
2023-01-31 17:05:38 +0300

    Задача 4 билета

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
+import java.util.Arrays;
+
 public class Main {
+    public static String removeDuplicatesAndConcatenate(String[] arr) {
+        String result = "";
+        for (int i = 0; i < arr.length; i++) {
+            boolean duplicate = false;
+            for (int j = 0; j < i; j++) {
+                if (arr[i].equals(arr[j])) {
+                    duplicate = true;
+                    break;
+                }
+            }
+            if (!duplicate) {
+                result += arr[i];
+            }
+        }
+        return result;
+    }
     public static void main(String[] args) {
+        String[] arr = new String[]{"ab", "ef", "cd", "ef", "gh", "ij", "kl", "ab", "mn"};
+        Arrays.sort(arr);
+        System.out.println(removeDuplicatesAndConcatenate(arr));
     }
 }
